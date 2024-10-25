8ca3a9673a6ebc08ae42df57591d2a90ad84e55d
surendran baiyanesh <113828371+surendran-180202@users.noreply.github.com>
2024-10-25 18:10:08 +0530

    Room Allegation Logic Task Init

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace RoomAllegation
+{
+    internal class Program
+    {
+        #region Privates
+        private static void Main(string[] args)
+        {
+            //Test Case 1
+            RoomAllegation(9, 0);
+
+            //Test Case 2
+            RoomAllegation(9, 3); //1+3, 1+3, 1+3  
+
+            //Test Case 3
+            RoomAllegation(9, 4); //1+3, 1+3, 1+3, 1+0  
+
+            //Test Case 4
+            RoomAllegation(6, 2); //1+3, 1+3
+
+            //Test Case 5
+            RoomAllegation(0, 2); //2+0,
+
+            //Test Case 6
+            RoomAllegation(0, 10); //4+0, 4+0, 2+0
+
+            //Test Case 7
+            RoomAllegation(10, 6); //1+3, 1+3, +0  
+
+            //Test Case 8
+            RoomAllegation(4, 12); //1+3, 3+1, 4+0, 4+0
+
+            //Test Case 9
+            RoomAllegation(11, 6); //Maximum Alowed 16 Total Member Count Morethen 16 So not acceptable
+
+            //Test Case 10
+            RoomAllegation(11, 2); //Adult And Child Ratio Not Equal
+
+            //Test Case 11
+            RoomAllegation(10, 4); //1+3, 1+3, 1+3, 1+1
+
+            Console.ReadKey();
+        }
+
+        private static void RoomAllegation(int nChild, int nAdult)
+        {
+            int nTotalMember = nChild + nAdult;
+
+            int nRooms = nTotalMember / 4;
+
+            if(nTotalMember % 4 != 0) nRooms++;
+
+            if(ErrorHandler(nChild, nAdult)) PreparePattern(nChild, nAdult, nRooms);
+
+            Console.WriteLine("________________________");
+        }
+
+        private static bool ErrorHandler(int nChild, int nAdult)
+        {
+            if(nAdult == 0)
+            {
+                Console.WriteLine("Adult 0 not acceptable");
+                return false;
+            }
+
+            if(nChild + nAdult > 16)
+            {
+                Console.WriteLine("Maximum Alowed 16 Total Member Count More Then 16 not acceptable");
+                return false;
+            }
+
+            if(nChild > nAdult * 3)
+            {
+                Console.WriteLine("Adult And Child Ratio Not Equal");
+                return false;
+            }
+
+            return true;
+        }
+
+        private static void PreparePattern(int nChildCount, int nAdultCount, int nTotalRoom)
+        {
+            int nTempChildCount = nChildCount;
+            int nTempAdultCount = nAdultCount;
+            int nTempRoomsCount = nTotalRoom;
+
+            for(int i = 0; i < nTempRoomsCount; i++)
+            {
+                if(nTempChildCount >= 3)
+                {
+                    Console.WriteLine("Adult 1 Child 3");
+
+                    nTempChildCount -= 3;
+                    nTempAdultCount--;
+                }
+                else if(nTempChildCount > 0)
+                {
+                    int nTemp = 4 - nTempChildCount;
+
+                    Console.WriteLine($"Adult {nTemp} Child {nTempChildCount}");
+
+                    nTempAdultCount -= nTemp;
+                    nTempChildCount -= nTempChildCount;
+                }
+                else
+                {
+                    int nTemp = nTempAdultCount > 4 ? 4 : nTempAdultCount;
+                    Console.WriteLine($"Adult {nTemp} Child 0");
+
+                    nTempAdultCount -= 4;
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RoomAllegation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RoomAllegation")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2150cc90-49af-4280-8538-a5c47a4bddaa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
